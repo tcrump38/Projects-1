e34bf351dddc8dff4f0f578cf18655e6b85e8ef2
tcuddle <thcrump38@yahoo.com>
2018-11-15 19:32:22 -0600

    added script tags and cdn

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCACl0dIADoUYtr0cU0l6WsUMGbcHhC3Vo",
+    authDomain: "project1-388e6.firebaseapp.com",
+    databaseURL: "https://project1-388e6.firebaseio.com",
+    projectId: "project1-388e6",
+    storageBucket: "project1-388e6.appspot.com",
+    messagingSenderId: "62443097997"
+  };
+  firebase.initializeApp(config);
