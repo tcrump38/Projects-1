95bbf4f4eabc315aa483c7f24fd67012dbed6e79
sheylao <sheypr85@gmail.com>
2018-11-17 12:16:11 -0600

    Adding JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 
-  
   // Changes DOM with Materialize
   $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
     });
+
 
 var config = {
     apiKey: "AIzaSyCACl0dIADoUYtr0cU0l6WsUMGbcHhC3Vo",
@@ -16,5 +16,36 @@ var config = {
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
+
+      // Firebase watcher + initial loader HINT: .on("value")
+      database.ref('/beersBlueOwl').on("value", function(snapshot) {
+
+        var results = snapshot.val()
+        
+                for (var i = 0; i < results.length; i++) {
+                    var showBeers = $("<div>");
+
+                    //gets the rating from giphy data and puts it in a p tag
+                    var beerName = results[i].name;
+                    var p = $("<p>").text("Name " + beerName);
+                    showBeers.append(beerName)
 
 
+
+                    $("#Beer-info").append(showBeers)
+
+                }
+        // Log everything that's coming out of snapshot
+        console.log(snapshot.val());
+        
+  
+
+         
+
+  
+        // Handle the errors
+      }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+      });
+  
