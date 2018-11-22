42371e0436fa4e0b6138243c2553a7f1ad23a8f2
Hector M <moralh2@gmail.com>
2018-11-21 22:24:16 -0600

    fix table

--- table.js
+++ table.js
@@ -17,19 +17,27 @@ $(document).ready(function () {
   
   var database = firebase.database();
   var breweriesATX;
+
   
   // Firebase watcher + initial loader HINT: .on("value")
   database.ref('/breweriesJSON').on("value", function (snapshot) {
     var results = snapshot.val()
+    breweriesATX = results
     for (var i = 0; i < results.length; i++) {
       var showBeers = $("<tr>")
-      var beerName = $("<td>").text(results[i].name)
-    //   var beerLoc = $("<td>").text(results[i].location)
-      var beerHours = $("<td>").text(results[i].hours)
-      var beerCount = $("<td>").text(results[i].amountOfBeers)
-      var beerTypes = $("<td>").text(results[i].typesOfBeer)
-      showBeers.append(beerName).append(beerHours).append(beerCount).append(beerTypes)
-      $("#table-body").append(showBeers)
+      if (typeof results[i] == "undefined") {
+        console.log('skipped')
+      }
+      else {
+        var beerName = $("<td>").text(results[i].name)
+        //   var beerLoc = $("<td>").text(results[i].location)
+          var beerHours = $("<td>").text(results[i].hours)
+          var beerCount = $("<td>").text(results[i].amountOfBeers)
+          var beerTypes = $("<td>").text(results[i].typesOfBeer)
+          showBeers.append(beerName).append(beerHours).append(beerCount).append(beerTypes)
+          $("#table-body").append(showBeers)
+      }
+
   }
     // Log everything that's coming out of snapshot
     console.log(snapshot.val());
